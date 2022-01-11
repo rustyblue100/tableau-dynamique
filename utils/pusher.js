9a8a0ddee8d3f547fb855a6a0a77c40567a70bc5
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-01-11 00:45:16 -0500

    changed: .env keys on pusher instance

--- utils/pusher.js
+++ utils/pusher.js
@@ -1,9 +1,9 @@
 const Pusher = require("pusher");
 
 export const pusher = new Pusher({
-  appId: process.env.pusherId,
-  key: process.env.pusherKey,
-  secret: process.env.pusherSecret,
+  appId: process.env.NEXT_PUBLIC_PUSHERID,
+  key: process.env.NEXT_PUBLIC_PUSHERKEY,
+  secret: process.env.NEXT_PUBLIC_PUSHERSECRET,
   cluster: "us2",
   useTLS: true,
 });
