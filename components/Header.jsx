075003ecb5f04a5dc675ecb3856470c80f66038f
Sebastien Francisque <sebastienfrancisque@seb-mac-laptop.local>
2022-03-28 23:56:37 -0400

    styled: padding on messaga à tous + pre-wrap

--- components/Header.jsx
+++ components/Header.jsx
@@ -158,7 +158,8 @@ const Header = ({ message, auth, value, setValue, setTabIndex }) => {
               sx={{
                 fontSize: { xs: 11, lg: 18 },
                 /*      borderRadius: 1, */
-                padding: "10px 5px",
+                padding: "10px 8px",
+                whiteSpace: "pre-wrap",
               }}
             >
               {message[0].fields.Message}
